1614d12c1b0e4701edc56d7401c0bfe0d3aba9f9
kawa <katarzyna.walenko@21s.pl>
2025-12-19 18:17:18 +0100

    App initialization

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html'
+  ],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    styled: true,
+  },
+};
