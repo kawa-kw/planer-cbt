f07f2385d921a07554ac38d78de953a051da8de5
kawa <katarzyna.walenko@21s.pl>
2025-12-20 19:55:37 +0100

    #8: add login feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import Lockdown from './components/Lockdown';
 import MainPage from './components/MainPage';
 
 function App() {
   return (
-    <Lockdown>
-      <MainPage />
-    </Lockdown>
+    <MainPage />
   );
 }
 
